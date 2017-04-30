9e29e1f9bac7a42a1b1ac514ab81bd4be9d375b9
Rasuli Ramin <grantik03@gmail.com>
2017-05-01 01:23:10 +0800

    Bug-fix

--- src/main/java/com/rr/Main.java
+++ src/main/java/com/rr/Main.java
@@ -1,16 +1,9 @@
 package com.rr;
 
-import com.rr.model.Student;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.stage.Stage;
-import org.hibernate.annotations.common.reflection.java.generics.TypeUtils;
-
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 
 public class Main extends Application {
 
