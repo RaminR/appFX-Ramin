cf03319aa1ad75fe18c7ebc7c0ee905e37abc42c
Rasuli Ramin <grantik03@gmail.com>
2017-05-05 20:11:32 +0800

    Unable to build Hibernate SessionFactory

--- root/rest/src/main/java/ru/esstu/ws/StudentResource.java
+++ root/rest/src/main/java/ru/esstu/ws/StudentResource.java
@@ -1,8 +1,10 @@
 package ru.esstu.ws;
 
 import ru.esstu.se.model.Student;
+import ru.esstu.se.service.impl.StudentServiceImpl;
 import ru.esstu.ws.service.StudentService;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -10,14 +12,37 @@ import java.util.List;
 @Path("/student")
 public class StudentResource {
 
+//    private final StudentServiceImpl studentService;
+//
+//    @Inject
+//    public StudentResource(StudentServiceImpl studentService) {
+//        this.studentService = studentService;
+//    }
+//
+//
+//    @GET
+//    @Produces
+//    public Student getAll() {
+//        return (Student) studentService.getAll();
+//    }
+//
+//    @GET
+//    @Path(value = "s/{id}")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Student getStudent(@PathParam("id") int id) {
+//        return (Student) studentService.getById(id);
+//    }
+
     @GET
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_XML)
     public List<Student> getAllStudent() {
-        List<Student> students = StudentService.getAll();
+        List<Student> students = StudentService.get();
         if (students == null) {
             throw new RuntimeException("Студентов нету!");
         }
         return students;
     }
+
 
 }
--- root/rest/src/main/java/ru/esstu/ws/service/StudentService.java
+++ root/rest/src/main/java/ru/esstu/ws/service/StudentService.java
@@ -7,21 +7,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StudentService {
-    private static StudentServiceImpl studentService = new StudentServiceImpl();
-//    private static
 
     public static List<Student> getAll() {
-
-//        students = studentService.getAll();
-//        List<Student> studentList = new ArrayList<>();
-//        for (Student student : students)
-//            studentList.add(new Student(student.getId(), student.getFirstname(), student.getSurname()));
-//
-//        return studentList;
         List<Student> students = new ArrayList<>();
-        Student student = new Student(1, "as", "as");
-        students.add(student);
+        students.add(new Student(1, "Имя", "Фамилия"));
+        students.add(new Student(2, "Имя", "Фамилия"));
+        students.add(new Student(3, "Имя", "Фамилия"));
         return students;
     }
 
+    private static List<Student> studentList = new ArrayList<>();
+    private static StudentServiceImpl studentService = new StudentServiceImpl();
+
+    public static List<Student> get() {
+        studentList = studentService.getAll();
+        return studentList;
+    }
 }
